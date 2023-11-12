53c1d4d1f32ff439167692eff1e382481d8f8bb0
iHesh <1058647@hr.nl>
2023-11-12 19:33:19 +0100

    refactor http calling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -34,6 +34,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState("");
   const [isSubmitting, setSubmitting] = useState(false);
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occured.");
+    }
+  })
+
   return (
     <div className="max-w-xl space-y-3">
       {error && (
@@ -42,16 +53,7 @@ const NewIssuePage = () => {
         </CalloutRoot>
       )}
       <form
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpected error occured.");
-          }
-        })}
+        onSubmit={onSubmit}
       >
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
